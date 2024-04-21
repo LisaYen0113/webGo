382ec9778600a74fea34b71aa0ed9a2f0a6591e8
kiki940430 <k0981183016@gmail.com>
2024-04-21 17:30:08 +0800

    421 530

---
+++ js_dyxC50KN5F1MJQqpgnF4FKZC-yisn2F7DhBpGQY3kzc.js
@@ -0,0 +1,81 @@
+Drupal.locale = { 'pluralFormula': function ($n) { return Number(($n>1)); }, 'strings': {"":{"An AJAX HTTP error occurred.":"\u767c\u751f\u4e00\u500b AJAX HTTP \u932f\u8aa4\u3002","HTTP Result Code: !status":"HTTP \u7d50\u679c\u78bc\uff1a!status","An AJAX HTTP request terminated abnormally.":"\u6709\u500b\u4e0d\u6b63\u5e38\u7d50\u675f\u7684 AJAX HTTP \u8cc7\u6e90\u8acb\u6c42","Debugging information follows.":"\u9644\u4e0a\u5075\u932f\u8cc7\u8a0a\u3002","Path: !uri":"\u8def\u5f91\uff1a!uri","StatusText: !statusText":"\u72c0\u614b\u6587\u5b57\uff1a!statusText","ResponseText: !responseText":"\u56de\u61c9\u6587\u5b57\uff1a!responseText","ReadyState: !readyState":"ReadyState: !readyState","Next":"\u4e0b\u4e00\u9801","Disabled":"\u505c\u7528","Enabled":"\u555f\u7528","Edit":"\u7de8\u8f2f","Sunday":"\u661f\u671f\u65e5","Monday":"\u661f\u671f\u4e00","Tuesday":"\u661f\u671f\u4e8c","Wednesday":"\u661f\u671f\u4e09","Thursday":"\u661f\u671f\u56db","Friday":"\u661f\u671f\u4e94","Saturday":"\u661f\u671f\u516d","Add":"\u65b0\u589e","Upload":"\u4e0a\u8f09","Configure":"\u8a2d\u7f6e","Done":"\u5b8c\u6210","Logo":"Logo","Site name":"\u7db2\u7ad9\u540d\u7a31","Site slogan":"\u7db2\u7ad9\u53e3\u865f","This field is required.":"\u6b64\u6b04\u4f4d\u662f\u5fc5\u9700\u7684\u3002","Prev":"\u4e0a\u4e00\u9801","Mon":"\u9031\u4e00","Tue":"\u4e8c","Wed":"\u4e09","Thu":"\u56db","Fri":"\u9031\u4e94","Sat":"\u9031\u516d","Sun":"\u9031\u66f0","January":"\u4e00\u6708","February":"\u4e8c\u6708","March":"\u4e09\u6708","April":"\u56db\u6708","May":"\u4e94\u6708","June":"\u516d\u6708","July":"\u4e03\u6708","August":"\u516b\u6708","September":"\u4e5d\u6708","October":"\u5341\u6708","November":"\u5341\u4e00\u6708","December":"\u5341\u4e8c\u6708","Show":"\u986f\u793a","Allowed HTML tags":"\u53ef\u4f7f\u7528\u7684 HTML \u6a19\u7c64","Select all rows in this table":"\u9078\u53d6\u8868\u683c\u4e2d\u7684\u6240\u6709\u5217","Deselect all rows in this table":"\u53d6\u6d88\u9078\u53d6\u8868\u683c\u4e2d\u7684\u6240\u6709\u5217","Today":"\u4eca\u5929","Jan":"1 \u6708","Feb":"2 \u6708","Mar":"3 \u6708","Apr":"4 \u6708","Jun":"6 \u6708","Jul":"7 \u6708","Aug":"8 \u6708","Sep":"9\u6708","Oct":"10 \u6708","Nov":"11 \u6708","Dec":"12 \u6708","Su":"\u65e5","Mo":"\u4e00","Tu":"\u4e8c","We":"\u4e09","Th":"\u56db","Fr":"\u4e94","Sa":"\u516d","Left":"\u5de6\u908a","Right":"\u53f3\u908a","Not published":"\u672a\u767c\u8868","Color scheme":"\u914d\u8272\u65b9\u6848","Please wait...":"\u8acb\u7a0d\u7b49...","Hide":"\u96b1\u85cf","Loading":"\u8f09\u5165\u4e2d","mm\/dd\/yy":"\u6708\u6708\/\u65e5\u65e5\/\u5e74\u5e74","Only files with the following extensions are allowed: %files-allowed.":"\u50c5\u5141\u8a31\u4e0b\u5217\u526f\u6a94\u540d\uff1a%files-allowed\u3002","Main menu":"\u4e3b\u9078\u55ae","By @name on @date":"\u7531 @name \u65bc @date \u767c\u8868","By @name":"\u4f9d\u540d\u7a31","Not in menu":"\u4e0d\u5b58\u5728\u9078\u55ae\u4e2d","Alias: @alias":"\u5225\u540d\uff1a@alias","No alias":"\u6c92\u6709\u8def\u5f91\u5225\u540d","New revision":"\u5efa\u7acb\u4fee\u8a02\u7248\u672c","Drag to re-order":"\u6293\u53d6\u9805\u76ee\u9032\u884c\u91cd\u65b0\u6392\u5217","Changes made in this table will not be saved until the form is submitted.":"\u5728\u9001\u51fa\u9019\u500b\u8868\u55ae\u4e4b\u524d\uff0c\u9019\u500b\u8868\u683c\u4e2d\u7684\u8b8a\u66f4\u4e0d\u6703\u88ab\u5132\u5b58\u3002","The changes to these blocks will not be saved until the \u003Cem\u003ESave blocks\u003C\/em\u003E button is clicked.":"\u9019\u4e9b\u5340\u584a\u7684\u8b8a\u66f4\u4e0d\u6703\u88ab\u5132\u5b58\u8d77\u4f86\uff0c\u9664\u975e\u60a8\u6309\u4e0b\u003Cem\u003E\u5132\u5b58\u5340\u584a\u003C\/em\u003E \u7684\u6309\u9215\u3002","User pictures in posts":"\u6587\u7ae0\u88e1\u7684\u4f5c\u8005\u5716\u7247","User pictures in comments":"\u56de\u61c9\u88e1\u7684\u4f5c\u8005\u5716\u7247","Shortcut icon":"\u5c0f\u5716\u793a (favicon)","Show shortcuts":"\u986f\u793a\u6377\u5f91","This permission is inherited from the authenticated user role.":"\u6b64\u6b0a\u9650\u7e7c\u627f\u81ea\u8a3b\u518a\u4f7f\u7528\u8005\u89d2\u8272\u3002","Secondary menu":"\u6b21\u9078\u55ae","No revision":"\u6c92\u6709\u4fee\u8a02\u7248\u672c","@number comments per page":"\u6bcf\u9801\u56de\u61c9\u6578\u91cf @number\u500b","Requires a title":"\u9700\u8981\u586b\u5beb\u6a19\u984c","Not restricted":"\u672a\u53d7\u9650\u5236","(active tab)":"(\u4f5c\u7528\u4e2d\u9801\u7c64)","Not customizable":"\u7121\u6cd5\u81ea\u8a02","Restricted to certain pages":"\u53d7\u9650\u65bc\u7279\u5b9a\u9801\u9762","The block cannot be placed in this region.":"\u7121\u6cd5\u628a\u5340\u584a\u653e\u5230\u9019\u500b\u5340\u57df\u3002","Customize dashboard":"\u81ea\u8a02\u5100\u8868\u677f","Hide summary":"\u96b1\u85cf\u6458\u8981","Edit summary":"\u7de8\u8f2f\u6458\u8981","Don\u0027t display post information":"\u4e0d\u8981\u986f\u793a\u6587\u7ae0\u7684\u5f35\u8cbc\u8cc7\u8a0a","@title dialog":"@title \u5c0d\u8a71\u65b9\u584a","User verification status in comments":"\u5728\u56de\u61c9\u4e2d\u7684\u4f7f\u7528\u8005\u9a57\u8b49\u72c0\u614b","The selected file %filename cannot be uploaded. Only files with the following extensions are allowed: %extensions.":"\u9078\u64c7\u7684\u6a94\u6848%filename\u7121\u6cd5\u4e0a\u8f09\u3002\u53ea\u6709\u4ee5\u4e0b\u985e\u578b\u7684\u6a94\u6848\u88ab\u5141\u8a31\uff1a%extensions\u3002","Re-order rows by numerical weight instead of dragging.":"\u900f\u904e\u6578\u5b57\u6b0a\u91cd\u65b9\u5f0f\u4f86\u9032\u884c\u91cd\u65b0\u6392\u5e8f\u3002","Show row weights":"\u986f\u793a\u5217\u6b04\u6b0a\u91cd","Hide row weights":"\u96b1\u85cf\u5217\u6b04\u6b0a\u91cd","Autocomplete popup":"\u81ea\u52d5\u5b8c\u6210\u5feb\u986f","Searching for matches...":"\u6b63\u5728\u67e5\u627e\u5339\u914d\u9805...","Hide shortcuts":"\u96b1\u85cf\u6377\u5f91"}} };;
+(function($) {
+  Drupal.behaviors.custom_search = {
+    attach: function(context) {
+
+      if (!Drupal.settings.custom_search.solr) {
+        // Check if the search box is not empty on submit
+        $('form.search-form', context).submit(function(){
+          var $this = $(this);
+          var box = $this.find('input.custom-search-box');
+          if (box.val() != undefined && box.val() == '') {
+            $this.find('input.custom-search-box').addClass('error');
+            return false;
+          }
+          // If basic search is hidden, copy or value to the keys
+          if ($this.find('#edit-keys').parents('div.element-invisible').attr('class') == 'element-invisible') {
+            $this.find('#edit-keys').val($this.find('#edit-or').val());
+            $this.find('#edit-or').val('');
+          }
+          return true;
+        });
+      }
+
+      // Search from target
+      $('form.search-form').attr('target', Drupal.settings.custom_search.form_target);
+
+      // Displays Popup.
+      $('form.search-form input.custom-search-box', context).bind('click focus', function(e){
+        var $parentForm = $(this).parents('form');
+        // check if there's something in the popup and displays it
+        var popup = $parentForm.find('fieldset.custom_search-popup');
+        if (popup.find('input,select').length && !popup.hasClass('opened')) {
+          popup.fadeIn().addClass('opened');
+        }
+        e.stopPropagation();
+      });
+      $(document).bind('click focus', function(){
+        $('fieldset.custom_search-popup').hide().removeClass('opened');
+      });
+
+      // Handle checkboxes
+      $('.custom-search-selector input:checkbox', context).each(function(){
+        var el = $(this);
+        if (el.val() == 'c-all') {
+          el.change(function(){
+            $(this).parents('.custom-search-selector').find('input:checkbox[value!=c-all]').attr('checked', false);
+          });
+        }
+        else {
+          if (el.val().substr(0,2) == 'c-') {
+            el.change(function(){
+              $('.custom-search-selector input:checkbox').each(function(){
+                if ($(this).val().substr(0,2) == 'o-') {
+                  $(this).attr('checked', false);
+                }
+              });
+              $(this).parents('.custom-search-selector').find('input:checkbox[value=c-all]').attr('checked', false);
+            });
+          } else {
+            el.change(function(){
+              $(this).parents('.custom-search-selector').find('input:checkbox[value!=' + el.val() + ']').attr('checked', false);
+            });
+          }
+        }
+      });
+
+      // Handle popup.
+      var popup = $('fieldset.custom_search-popup:not(.custom_search-processed)', context).addClass("custom_search-processed");
+      popup.click(function(e){
+        e.stopPropagation();
+      })
+      popup.append('<a class="custom_search-popup-close" href="#">' + Drupal.t('Close') + '</a>');
+      $('a.custom_search-popup-close').click(function(e){
+        $('fieldset.custom_search-popup.opened').hide().removeClass('opened');
+        e.preventDefault();
+      });
+
+    }
+  }
+})(jQuery);
+;
